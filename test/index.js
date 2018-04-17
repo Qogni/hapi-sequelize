ed9ff4181f728f72c505d9f8bd3b60f1b881cec0
Rodrigo Saboya <saboya@users.noreply.github.com>
2018-04-17 03:09:21 -0300

    Fixing test for new package name.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Pkg = require('../package.json')
 const Lab = require('lab')
 const Code = require('code')
 const Sinon = require('sinon')
@@ -39,10 +40,10 @@ lab.suite('hapi-sequelize', () => {
         ],
       }))
 
-      expect(server.plugins['hapi-sequelize']['shop'].sequelize).to.be.an.instanceOf(Sequelize)
+      expect(server.plugins[Pkg.name]['shop'].sequelize).to.be.an.instanceOf(Sequelize)
       expect(spy.getCall(0).args[0]).to.be.an.instanceOf(Sequelize)
 
-      let tables = await server.plugins['hapi-sequelize']['shop'].sequelize.query('SELECT name FROM sqlite_master WHERE type=\'table\'', { type: Sequelize.QueryTypes.SELECT })
+      let tables = await server.plugins[Pkg.name]['shop'].sequelize.query('SELECT name FROM sqlite_master WHERE type=\'table\'', { type: Sequelize.QueryTypes.SELECT })
       expect(tables.filter(table => !table.startsWith('sqlite_')).length).to.equal(6)
     } catch (err) {
       expect(err).to.not.exist()
